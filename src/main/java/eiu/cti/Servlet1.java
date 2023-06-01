7f2959b0dce61f1c9064a4026eea5a12661fa9e5
Manuel Clavel <manuelgclavel@gmail.com>
2023-06-01 11:30:17 +0700

    servlet1

--- src/main/java/eiu/cti/Servlet1.java
+++ src/main/java/eiu/cti/Servlet1.java
@@ -3,12 +3,14 @@ package eiu.cti;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;  
   
-  
+
+@WebServlet(name = "Servlet1", urlPatterns = { "/servlet1" }, loadOnStartup = 1)
 public class Servlet1 extends HttpServlet {  
   
   /**
@@ -25,7 +27,7 @@ public void doGet(HttpServletRequest request, HttpServletResponse response) thro
  
   
     	//response.addHeader("Access-Control-Allow-Origin", "*");
-		CookieHeader.createSetCookieHeader(response, "servlet1", "servlet1 cookie", "www.tanbinhtech.com", "/", "None", true, false, 900);
+		CookieHeader.createSetCookieHeader(response, "servlet1", "servlet1 cookie", "13.229.227.163:8080", "/", "None", true, false, 900);
     	response.addHeader("Access-Control-Allow-Credentials", "true");
         }catch(Exception e){System.out.println(e);}  
   } 
