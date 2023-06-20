6b77f3c42b60b7fd7230a530adcad26a77d4a5fa
Manuel Clavel <manuelgclavel@gmail.com>
2023-06-20 15:19:07 +0700

    working

--- src/main/java/eiu/cti/ServletCheck.java
+++ src/main/java/eiu/cti/ServletCheck.java
@@ -29,9 +29,11 @@ public class ServletCheck extends HttpServlet {
 			//			  "www.tanbinhtech.com", "/", "None",
 			//			  false, false, 900);
 			response.addHeader("Access-Control-Allow-Credentials", "true");
-			response.addHeader("Access-Control-Allow-Origin", "*");
-			//response.addHeader("Access-Control-Allow-Origin",
-			//		   "http://localhost:8080");
+			//response.addHeader("Access-Control-Allow-Origin", "*");
+			//Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at ‘https://www.tanbinhtech.com/cookietester/check’. 
+			//	(Reason: Credential is not supported if the CORS header ‘Access-Control-Allow-Origin’ is ‘*’).
+			response.addHeader("Access-Control-Allow-Origin",
+					   "http://localhost:8080");
 			StringBuilder message = new StringBuilder();
 			message.append("hi");
 			Writer writer = response.getWriter();
