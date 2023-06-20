0b33fb7806a851e036280f6c3226f0d2e2eb9e73
Manuel Clavel <manuelgclavel@gmail.com>
2023-06-20 23:06:16 +0700

    working

--- src/main/java/eiu/cti/Servlet1.java
+++ src/main/java/eiu/cti/Servlet1.java
@@ -26,7 +26,8 @@ public class Servlet1 extends HttpServlet {
 			response.addHeader("Access-Control-Allow-Credentials", "true");
 			// response.addHeader("Access-Control-Allow-Origin", "*");
 			response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-			response.setHeader("Set-Cookie", "servlet1=servlet1;"); // Domain=www.tanbinhtech.com; Path=/check; SameSite=None; Secure; Max-Age=900;");
+			response.setHeader("Set-Cookie", "servlet1=servlet1; Domain=www.tanbinhtech.com; Path=/cookietester; SameSite=None; Max-Age=600;");
+			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
