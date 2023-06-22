ad50cf253778492f00e85b465d6876ce5f57f016
Manuel Clavel <manuelgclavel@gmail.com>
2023-06-22 14:16:29 +0700

    working

---
+++ src/main/java/eiu/cti/Servlet2lk.java
@@ -0,0 +1,30 @@
+package eiu.cti;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Servlet2", urlPatterns = { "/servlet2" }, loadOnStartup = 1)
+public class Servlet2lk extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		try {
+			response.setContentType("text/html");
+			//CookieHeader.createSetCookieHeader(response, "servlet2", "servlet2 cookie", "www.tanbinhtech.com", "/",
+			//		"Lax", true, false, 900);
+			response.setHeader("Set-Cookie", "cookie2lk=cookie2lk; Domain=www.tanbinhtech.com; Path=/cookietester; SameSite=Lax; Secure; Max-Age=600;");
+			
+			response.addHeader("Access-Control-Allow-Credentials", "true");
+			response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
---
+++ src/main/java/eiu/cti/Servlet3lk.java
@@ -0,0 +1,30 @@
+package eiu.cti;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Servlet3", urlPatterns = { "/servlet3" }, loadOnStartup = 1)
+public class Servlet3lk extends HttpServlet {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		try {
+			response.setContentType("text/html");
+			//CookieHeader.createSetCookieHeader(response, "servlet3", "servlet3", "www.tanbinhtech.com", "/",
+			//		"Strict", true, false, 900);
+			response.setHeader("Set-Cookie", "cookie3lk=cookie3lk; Domain=www.tanbinhtech.com; Path=/cookietester; SameSite=Strict; Secure; Max-Age=600;");
+			
+			response.addHeader("Access-Control-Allow-Credentials", "true");
+			response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
