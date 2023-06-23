9c295167c1ef007a43b063ffe39b481f1650ca47
Manuel Clavel <manuelgclavel@gmail.com>
2023-06-23 08:08:37 +0700

    working

--- src/main/java/eiu/cti/ServletRemove.java
+++ src/main/java/eiu/cti/ServletRemove.java
@@ -7,34 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ServletRemove {
-	
-	@WebServlet(name = "ServletRemove", urlPatterns = { "/remove" }, loadOnStartup = 1)
-	public class Servlet1 extends HttpServlet {
+@WebServlet(name = "ServletRemove", urlPatterns = { "/remove" }, loadOnStartup = 1)
+public class ServletRemove extends HttpServlet {
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
 
-		public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-			
-			response.setContentType("text/plain");
+	private static final long serialVersionUID = 1L;
 
-			try {
-				response.addHeader("Access-Control-Allow-Credentials", "true");
-				// response.addHeader("Access-Control-Allow-Origin", "*");
-				response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-				response.setHeader("Set-Cookie", "cookie1=cookie1; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
-				response.setHeader("Set-Cookie", "cookie1b=cookie1b; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
-				response.setHeader("Set-Cookie", "cookie2=cookie2; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
-				response.setHeader("Set-Cookie", "cookie3=cookie3; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
-				
-			} catch (Exception e) {
-				System.out.println(e);
-			}
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		try {
+			response.addHeader("Access-Control-Allow-Credentials", "true");
+			response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+			response.setHeader("Set-Cookie",
+					"cookie1=cookie1; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
+			response.setHeader("Set-Cookie",
+					"cookie1b=cookie1b; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
+			response.setHeader("Set-Cookie",
+					"cookie2=cookie2; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
+			response.setHeader("Set-Cookie",
+					"cookie3=cookie3; Domain=www.tanbinhtech.com; Path=/cookietester; Max-Age=0;");
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-
 	}
-
 }
